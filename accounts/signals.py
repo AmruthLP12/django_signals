d8485ef536fa334b812e22c13227cd0085be0069
Amruthlp <amruthlp12@gmail.com>
2026-07-04 19:43:51 +0530

    Add profile creation, bio formatting, and deletion logging signals - Django signals 3

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,pre_save,post_delete
 from django.dispatch import receiver
 
 from django.contrib.auth.models import User
@@ -9,3 +9,13 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        
+@receiver(pre_save, sender=Profile)
+def uppercase_bio(sender, instance, **kwargs):
+    
+    if instance.bio:
+        instance.bio = instance.bio.upper()
+        
+@receiver(post_delete, sender=Profile)
+def profile_deleted(sender, instance, **kwargs):
+    print(f'Profile for user {instance.user.username} has been deleted.')
