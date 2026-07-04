ba9635a5c4ee2d124efbec0878ead4435c39191b
Amruthlp <amruthlp12@gmail.com>
2026-07-04 21:39:12 +0530

    Add custom signal for successful payments and implement related functionality Part  4

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,21 +1,30 @@
-from django.db.models.signals import post_save,pre_save,post_delete
+from django.db.models.signals import post_save, pre_save, post_delete
+from .custom_signals import payment_successfull
 from django.dispatch import receiver
 
 from django.contrib.auth.models import User
 
 from .models import Profile
 
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        
+
+
 @receiver(pre_save, sender=Profile)
 def uppercase_bio(sender, instance, **kwargs):
-    
+
     if instance.bio:
         instance.bio = instance.bio.upper()
-        
+
+
 @receiver(post_delete, sender=Profile)
 def profile_deleted(sender, instance, **kwargs):
-    print(f'Profile for user {instance.user.username} has been deleted.')
+    print(f"Profile for user {instance.user.username} has been deleted.")
+
+
+@receiver(payment_successfull)
+def payment_completed(sender, amount, user, **kwargs):
+    print(f"Payment of {amount} was successful for user {user.username}.")
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .custom_signals import payment_successfull
+
 
 # Create your views here.
+class PaymentView:
+    def make_payment(self, user, amount):
+        # Logic to process payment
+        # After successful payment, send the custom signal
+        payment_successfull.send(sender=self.__class__, amount=amount, user=user)
